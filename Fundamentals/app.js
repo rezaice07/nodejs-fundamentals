ae0b9194aab3128f94d478d4c1c8c6b1c3daeb91
Md. Rezazoyanul Islam <rezaice07@gmail.com>
2021-10-24 11:42:09 +0600

    added new files

---
+++ Fundamentals/app.js
@@ -0,0 +1,11 @@
+
+var Greeting=(greetingTo)=>{
+    console.log(`Greeting from ${greetingTo}`);
+}
+
+var MainFunction=(Greeting,greetingTo)=>{
+    console.log('MainFunction')
+    Greeting(greetingTo);
+}
+
+MainFunction(Greeting,'Rejwanul Reja');
