97afabe8c443182c6308ac71a48bd13d5b964ab4
Rejwanul Reja <rezaice07@gmail.com>
2021-12-12 09:57:37 +0600

    udpated token

--- LearnWithSumit/raw-node-app/handlers/routeHandlers/tokenHandler.js
+++ LearnWithSumit/raw-node-app/handlers/routeHandlers/tokenHandler.js
@@ -7,7 +7,7 @@
 
 // dependencies
 const data = require('../../lib/data');
-const { hash } = require('../../helpers/utilies');
+const { hash, createRandomString } = require('../../helpers/utilies');
 const { parseJSON } = require('../../helpers/utilies');
 
 // module scaffolding
@@ -18,9 +18,15 @@ const requestProps = {
     path: '',
     trimmedPath: '',
     method: '',
-    queryStringObject: '',
+    queryStringObject: {
+        id: '',
+    },
     heardObjects: '',
-    body: {},
+    body: {
+        id: '',
+        extend: false,
+    },
+    tokenId: '',
 };
 const acceptedMethods = ['get', 'post', 'put', 'delete'];
 
@@ -35,12 +41,135 @@ handler.tokenHandlerHandler = (reqProps, callBack) => {
 // service : token
 handler._tokens = {};
 
-handler._tokens.post = (reqProps = requestProps, callBack) => {};
+handler._tokens.post = (reqProps = requestProps, callBack) => {
+    const phone =
+        typeof reqProps.body.phone === 'string' && reqProps.body.phone.trim().length === 11
+            ? reqProps.body.phone
+            : false;
 
-handler._tokens.get = (reqProps = requestProps, callBack) => {};
+    const password =
+        typeof reqProps.body.password === 'string' && reqProps.body.password.length > 0
+            ? reqProps.body.password
+            : false;
 
-handler._tokens.put = (reqProps = requestProps, callBack) => {};
+    if (phone && password) {
+        data.read('users', phone, (err, res) => {
+            if (!err) {
+                const user = parseJSON(res);
+                const hashedPassword = hash(password);
+                if (hashedPassword === user.password) {
+                    // generate token
+                    const tokenId = createRandomString(120);
+                    const expires = Date.now() + 60 * 60 * 1000;
+                    const tokenObject = {
+                        phone,
+                        id: tokenId,
+                        expires,
+                    };
 
-handler._tokens.delete = (reqProps = requestProps, callBack) => {};
+                    // store the token to db
+                    data.create('tokens', tokenId, tokenObject, (err) => {
+                        if (!err) {
+                            callBack(200, { message: 'Token created successfully!' });
+                        } else {
+                            callBack(500, { error: 'Could not create token!' });
+                        }
+                    });
+                } else {
+                    callBack(400, {
+                        message: 'Password Is not valid.Try again',
+                    });
+                }
+            } else {
+                callBack(404, {
+                    message: 'You have a problem in your request. Please try again!',
+                });
+            }
+        });
+    } else {
+        callBack(404, { message: 'Invalid phone number. Please try again!' });
+    }
+};
+
+handler._tokens.get = (reqProps = requestProps, callBack) => {
+    // check the id if valid
+    const id =
+        typeof reqProps.queryStringObject.id === 'string' ? reqProps.queryStringObject.id : false;
+
+    if (id) {
+        // lookup the token
+        data.read('tokens', id, (err, res) => {
+            const tokenData = parseJSON(res);
+            if (!err && tokenData) {
+                callBack(200, { token: tokenData });
+            } else {
+                callBack(500, { error: 'There was an error on server side' });
+            }
+        });
+    } else {
+        callBack(500, { error: 'Request token not found.' });
+    }
+};
+
+handler._tokens.put = (reqProps = requestProps, callBack) => {
+    // check the id if valid
+    const id = typeof reqProps.body.id === 'string' ? reqProps.body.id : false;
+
+    const extend =
+        typeof reqProps.body.extend === 'boolean' && reqProps.body.extend === true
+            ? reqProps.body.extend
+            : false;
+    console.log(id && extend);
+    if (id && extend) {
+        data.read('tokens', id, (err, res) => {
+            if (!err) {
+                const tokenObject = parseJSON(res);
+                if (tokenObject.expires > Date.now()) {
+                    tokenObject.expires = Date.now() + 60 * 60 + 1000;
+
+                    // store data to database
+                    data.update('tokens', id, tokenObject, (err) => {
+                        if (!err) {
+                            callBack(200, { message: 'Token updaetd successfully' });
+                        } else {
+                            callBack(500, {
+                                message: 'Update Failed. Please try again!',
+                            });
+                        }
+                    });
+                } else {
+                    callBack(500, {
+                        message: 'Token Alredy expired!',
+                    });
+                }
+            } else {
+                callBack(404, {
+                    message: 'You have a problem in your request. Please try again!',
+                });
+            }
+        });
+    } else {
+        callBack(404, { message: 'Invalid token. Please try again!' });
+    }
+};
+
+handler._tokens.delete = (reqProps = requestProps, callBack) => {
+    const id =
+        typeof reqProps.body.id === 'string' && reqProps.body.id.trim().length > 0
+            ? reqProps.body.id
+            : false;
+
+    if (id) {
+        data.delete('tokens', id, (err) => {
+            if (!err) {
+                callBack(200, { message: 'Token Deleted successfully' });
+            } else {
+                callBack(500, { message: 'Error On Deleting Token. Please try again!' });
+            }
+        });
+    } else {
+        callBack(500, { message: 'Invalid Token. Please try again!' });
+    }
+};
 
 module.exports = handler;
--- LearnWithSumit/raw-node-app/helpers/utilies.js
+++ LearnWithSumit/raw-node-app/helpers/utilies.js
@@ -34,5 +34,26 @@ utilities.parseJSON = (jsonString) => {
     return output;
 };
 
+// create random string
+utilities.createRandomString = (strLen) => {
+    let length = strLen;
+    length = typeof strLen === 'number' && strLen > 0 ? strLen : false;
+
+    if (length) {
+        const possibleCharacters =
+            'galkglakgoi64gae87galkglakgoi64gae87ghr89bfd4bs4b897sh4s6h4s6h4s6hs887974s6h46s4hs6h4s6h48s97h89rewg46bs7r98gh65sfb4sb789sth56ghr89bfd4bs4b897sh4s6h4s6h4s6hs887974s6h46s4hs6h4s6h48s97h89rewg46bs7r98gh65sfb4sb789sth56galkglakgoi64gae87galkglakgoi64gae87ghr89bfd4bs4b897sh4s6h4s6h4s6hs887974s6h46s4hs6h4s6h48s97h89rewg46bs7r98gh65sfb4sb789sth56ghr89bfd4bs4b897sh4s6h4s6h4s6hs887974s6h46s4hs6h4s6h48s97h89rewg46bs7r98gh65sfb4sb789sth56';
+
+        let output = '';
+        for (let i = 1; i <= length; i += 1) {
+            const randNo = Math.floor(Math.random() * possibleCharacters.length);
+            const randomCharacter = possibleCharacters.charAt(randNo);
+            output += randomCharacter;
+        }
+
+        return output;
+    }
+    return false;
+};
+
 // export items
 module.exports = utilities;
