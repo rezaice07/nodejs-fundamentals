d073ecccff266e187202e92529e633128d9daf79
Rejwanul Reja <rezaice07@gmail.com>
2022-01-11 00:27:04 +0600

    updated basics

--- LearnWithSumit/express-fundamentals/index.js
+++ LearnWithSumit/express-fundamentals/index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+//app.use(express.raw());
+//app.use(express.json());
+//app.use(express.urlencoded());
+// app.use(express.static(`${__dirname}/public/`,{
+//     index:'home.html'
+// }))
 
-app.get('/', (req, res) => {
+const router = express.Router({caseSensitive:true});
+
+app.use(router);
+
+// app.get('/', (req, res) => {
+//     res.send('This is home page');
+// })
+
+// app.post('/', (req, res) => {
+//     console.log(req.body)
+//     res.send('This is home page with post request'); 
+// })
+
+
+router.get('/', (req, res) => {
     res.send('This is home page');
 })
 
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
     console.log(req.body)
     res.send('This is home page with post request'); 
 })
